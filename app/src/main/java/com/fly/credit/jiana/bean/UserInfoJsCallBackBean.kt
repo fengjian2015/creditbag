dd640ae61fcc7da9c118b99a8d822077ddc06fa1
feng <fengjiannattiness@163.com>
2024-03-20 22:42:59 +0800

    功能完善

---
+++ app/src/main/java/com/fly/credit/jiana/bean/UserInfoJsCallBackBean.kt
@@ -0,0 +1,12 @@
+package com.fly.credit.jiana.bean
+
+/**
+ * Time：2024/3/20
+ * Author：feng
+ * Description：
+ */
+class UserInfoJsCallBackBean :java.io.Serializable {
+    var devForCreditBagName : String? = null
+    var appForCreditBagVersion : String? = null
+    var loginForCreditBagData : String? =null
+}
--- app/src/main/java/com/fly/credit/jiana/js/InvokeForCreditBagUserInfo.kt
+++ app/src/main/java/com/fly/credit/jiana/js/InvokeForCreditBagUserInfo.kt
@@ -1,9 +1,13 @@
 package com.fly.credit.jiana.js
 
+import android.content.Context
 import android.webkit.WebView
+import com.fly.credit.jiana.MyApplication
+import com.fly.credit.jiana.bean.UserInfoJsCallBackBean
 import com.fly.credit.jiana.manage.UserInfoManage
 import com.fly.credit.jiana.util.Cons
 import com.fly.credit.jiana.web.AndroidCallBackJS
+import com.google.gson.Gson
 
 /**
  * Time：2024/3/14
@@ -13,9 +17,26 @@ import com.fly.credit.jiana.web.AndroidCallBackJS
 object InvokeForCreditBagUserInfo {
     //获取用户信息
     public fun invokeForCreditBagUserInfo(id: String,webView : WebView){
-
+        var userInfoJsCallBackBean = UserInfoJsCallBackBean()
+        userInfoJsCallBackBean.devForCreditBagName="android"
+        userInfoJsCallBackBean.loginForCreditBagData = UserInfoManage.getUserInfoJson()
+        userInfoJsCallBackBean.appForCreditBagVersion = getVersionName(MyApplication.application)
         AndroidCallBackJS.callBackJsSuccess(webView,id
             , Cons.INVOKEFORCREDITBAGUSERINFO
-            , UserInfoManage.getUserInfoJson())
+            , Gson().toJson(userInfoJsCallBackBean))
+    }
+
+    fun getVersionName(context: Context?): String {
+        if (context == null) {
+            return ""
+        }
+        var versionName = ""
+        try {
+            versionName =
+                context.packageManager.getPackageInfo(context.packageName, 0).versionName
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return versionName
     }
 }
